7ac654c15757dabbb12f608cfb7859d6b45d0d87
cognatus <diegojmartinezzm@gmail.com>
2017-04-04 21:21:10 -0500

    Estrcutura angular

---
+++ public/javascripts/controllers/registroCtrl.js
@@ -0,0 +1,23 @@
+(function() {
+
+	'use strict';
+
+	angular.module('aplicacion').controller('registroCtrl', registroController);
+	registroController.$inject = ['$scope','$document','$http', 'registroFactory'];
+
+
+	function registroController($scope, $document, $http, registroFactory){
+
+		var vm = $scope;
+		var fc = registroFactory;
+		
+		vm.registra = function(){
+			fc.registrar(vm.registro)
+			.success(function(data) {
+			})
+			.error(function(err) {
+			});
+		};
+
+	}
+})();
---
+++ public/javascripts/factories/registroFactory.js
@@ -0,0 +1,26 @@
+(function() {
+	'use strict';
+
+	angular.module('aplicacion').factory('registroFactory', registroF);
+	registroF.$inject = ['$http'];
+
+	function registroF($http){
+	    return {
+	    	registrar: function(datos) {
+	    		return $http({
+					method: 'POST',
+					url: '/registrar/',
+					data: {
+						nombre: 	datos.nombre,
+						curso: 		datos.curso,
+						edad: 		datos.edad,
+						email: 		datos.email,
+						telefono: 	datos.telefono,
+						escuela: 	datos.escuela
+					}
+				});
+	    	}
+	    }
+	}
+
+})();
